4a2f136e4fc8843e6ad96129b6b2f0e3c8faa30d
jundran <jundran@example.com>
2022-10-30 23:36:53 -0600

    Set up dynamic grid with styles, allowing user to input desired row/column count

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const container = document.querySelector('.container')
+const sketch = document.querySelector('.sketch')
+const numSquaresButton = document.querySelector('#numSquaresButton')
 
-const squares = []
+numSquaresButton.onclick = () => {
+  let ns = prompt("Enter number of squares per row / column (max 100)")
+  ns = parseInt(ns)
+  if(isNaN(ns) ) return
+  if(ns > 100) ns = 100
+  console.log(typeof ns, ns)
 
-for (let i = 0; i < 16; i++) {
-  const square = document.createElement('div')
-  square.classList.add("square")
-  squares.push(square)
-  container.appendChild(square)
+  generateSquares(
+    ns,
+    sketch.style.gridTemplateRows = `repeat(${ns}, 1fr)`,
+    sketch.style.gridTemplateColumns = `repeat(${ns}, 1fr)`
+  )
 }
+
+function generateSquares(num) {
+  sketch.innerHTML = ""
+
+  for (let i = 0; i < num**2; i++) {
+    const square = document.createElement('div')
+    square.classList.add("square")
+    
+    square.addEventListener('mouseover', e =>
+      e.target.classList.add('selected')
+    )
+    sketch.appendChild(square)
+  }
+}
+
+
