78a73141358353a4b396df07d28744d417a58cd4
jundran <jundran@example.com>
2022-10-31 10:46:26 -0600

    Add more buttons with functionality implemented for all but 'Erase Mode'

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## etch-a-sktech
-https://jundran.github.io/etch-a-sktech/
+https://jundran.github.io/etch-a-sketch/
 
 ## Description
 TOP - Etch-a-Sketch project
--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const sketch = document.querySelector('.sketch')
-const numSquaresButton = document.querySelector('#numSquaresButton')
+const sketch = document.querySelector('#sketch')
+const generateButton = document.querySelector('#generateButton')
+const clearButton = document.querySelector('#clearButton')
+const gridButton = document.querySelector('#gridButton')
+const eraseButton = document.querySelector('#eraseButton')
 
-numSquaresButton.onclick = () => {
-  let ns = prompt("Enter number of squares per row / column (max 100)")
-  ns = parseInt(ns)
-  if(isNaN(ns) ) return
-  if(ns > 100) ns = 100
-  console.log(typeof ns, ns)
+let gridShown = false
 
-  generateSquares(
-    ns,
-    sketch.style.gridTemplateRows = `repeat(${ns}, 1fr)`,
-    sketch.style.gridTemplateColumns = `repeat(${ns}, 1fr)`
-  )
-}
+generateButton.onclick = () => handleGenerateSquares()
+clearButton.onclick = () => clearCanvas()
+gridButton.onclick = () => handleGridButton()
+eraseButton.onclick = () => handleEraseButton()
 
 function generateSquares(num) {
   sketch.innerHTML = ""
@@ -27,6 +23,40 @@ function generateSquares(num) {
     )
     sketch.appendChild(square)
   }
+
+  // Newly rendered squares have border through css rule
+  gridShown = true
+  gridButton.textContent = 'Hide grid'
 }
 
+function handleGenerateSquares() {
+  let ns = prompt("Enter number of squares per row / column (max 100)")
+  ns = parseInt(ns)
+  if(isNaN(ns) ) return
+  if(ns > 100) ns = 100
+  console.log(typeof ns, ns)
 
+  generateSquares(
+    ns,
+    sketch.style.gridTemplateRows = `repeat(${ns}, 1fr)`,
+    sketch.style.gridTemplateColumns = `repeat(${ns}, 1fr)`
+  )
+}
+
+function clearCanvas() {
+  document.querySelectorAll('.selected').forEach(s => s.classList.remove('selected'))
+}
+
+function handleGridButton() {
+  if(gridShown) {
+    document.querySelectorAll('.square').forEach(s => s.style.border = `none`)
+  } else {
+    document.querySelectorAll('.square').forEach(s => s.style.border = `.25px solid #FFF`)
+  }
+  gridShown = !gridShown
+  gridButton.textContent = gridShown ? 'Hide grid' : 'Show grid'
+}
+
+function handleEraseButton() {
+
+}
